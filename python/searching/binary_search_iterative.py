d26701c3f2db21f8e8c1246b53a769d63f59325e
Mila Page <versusfacit@users.noreply.github.com>
2021-11-14 13:31:27 -0800

    Implement binary search array algorithm.

---
+++ python/searching/binary_search_iterative.py
@@ -0,0 +1,84 @@
+# Given a sorted array of n integers and a target value, determine if the
+# target exists in the array. If target exists in the array, print its index.
+
+# Time Complexity:
+#   Ω (1)           Target is the midpoint element.
+#   O(log(n))       Search space halves on each iteration.
+#   AVG_O(log(n))   About log(n) - 1; ~50% of outcomes before last iteration
+# Auxiliary Space: O(1) for variables only
+
+import unittest
+
+
+def binary_search(arr, target):
+    '''deal with edge cases before loop over list'''
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        curr = (low + high) // 2
+        if arr[curr] > target:
+            high = curr - 1
+        elif arr[curr] < target:
+            low = curr + 1
+        else:
+            return curr
+    else:
+        raise ValueError(f'Value {target} not found')
+
+
+class TestBinarySearch(unittest.TestCase):
+    '''test base cases'''
+    def test_binary_search_base_cases(self):
+        list1 = []
+        target1 = 1
+        with self.assertRaises(ValueError):
+            binary_search(list1, target1)
+
+        list2 = [1]
+        target2 = 1
+        self.assertEqual(0, binary_search(list2, target2))
+
+    '''test more complicated lists, including ones with negative numbers'''
+    def test_binary_search(self):
+        list1 = list(range(100))
+        target1 = 7
+        self.assertEqual(
+                list1.index(target1), binary_search(list1, target1))
+        target1b = 51
+        self.assertEqual(
+            list1.index(target1b), binary_search(list1, target1b)
+        )
+        target1c = 16
+        self.assertEqual(
+            list1.index(target1c), binary_search(list1, target1c)
+        )
+
+        list2 = [1, 5, 10, 11, 16, 35, 100, 300]
+        target2 = 10
+        self.assertEqual(
+            list2.index(target2), binary_search(list2, target2)
+        )
+
+        list3 = [-100, -123, -1, 5, 10, 11, 16, 35, 100, 300]
+        target3 = -1
+        self.assertEqual(
+            list3.index(target3), binary_search(list3, target3)
+        )
+
+    '''test for numeric lists that are missing target value'''
+    def test_binary_search_missing(self):
+        list1 = [1, 2, 3, 5, 6, 7, 8, 9, 10]
+        target1 = 4
+        with self.assertRaises(ValueError):
+            binary_search(list1, target1)
+
+        list2 = [1, 2, 3, 4, 6, 7, 8, 9, 10, 11]
+        target2 = 5
+        with self.assertRaises(ValueError):
+            binary_search(list2, target2)
+
+        list3 = [1, 2, 3, 4, 6, 7, 8, 9, 10, 11]
+        target3 = -1
+        with self.assertRaises(ValueError):
+            binary_search(list3, target3)
